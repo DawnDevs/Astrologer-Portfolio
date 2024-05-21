1e04ad906f849980b93340d9274edffa1a020b78
Sunil0881 <sunil12162003@gmail.com>
2024-05-21 21:21:33 +0530

    landing res

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -7,9 +7,9 @@ import '../styles/wheelAnimation.css'; // Import CSS file for wheel animation
 const LandingPage = () => {
   return (
     <div>
-      <main className="pt-16 xl:mb-4 lg:mb-10 xl:pb-5 md:flex justify-between md:pl-10 md:pr-7 lg:pl-16 lg:pr-10 md:items-center md:my-4">
-        <div className="md:flex-1 md:mr-10 px-10 ">
-          <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold mb-2 md:mb-4">
+      <main className="pt-16 xl:mb-4 lg:mb-10 xl:pb-5 md:flex justify-between md:pl-5 md:pr-5 lg:pl-16 lg:pr-10 md:items-center md:my-4">
+        <div className="md:flex-1  px-10 lg:pt-5">
+          <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold mb-2 md:mb-6">
             Got Any Questions?
           </h1>
           <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-black mb-2 md:mb-5">
@@ -25,7 +25,7 @@ const LandingPage = () => {
             CONNECT
           </button>   
         </div>
-        <div className="mt-5 md:mt-0 md:flex-1 hidden md:flex justify-center">
+        <div className="mt-5 md:mt-0  md:flex-1 hidden md:flex justify-center">
           <img src={wheel} height={500} width={500} alt="Astrology wheel" className="wheel-animation" />
         </div>
       </main>
