06eef923fe017625323fac27a4118741962379af
Sunil0881 <sunil12162003@gmail.com>
2024-05-26 20:56:38 +0530

    mode

--- frontend/src/pages/admin/Login.js
+++ frontend/src/pages/admin/Login.js
@@ -18,6 +18,7 @@ const Login = () => {
     try {
       const response = await fetch("https://astrologer-portfolio.vercel.app/api/login", {
         method: "POST",
+        mode: 'no-cors',
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
