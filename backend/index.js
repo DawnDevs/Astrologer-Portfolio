be70226317effb9cd87800f8e02dcf21020ccaef
Sunil0881 <sunil12162003@gmail.com>
2024-05-26 20:08:21 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json());
 
-const corsOptions = {
-  origin: 'https://astrologer-portfolio-client.vercel.app/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: 'https://astrologer-portfolio-client.vercel.app/',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
 
-app.use(cors(corsOptions)); // Apply CORS middleware globally
+app.use(cors()); // Apply CORS middleware globally
 
 // Connect to MongoDB
 mongoose
